3fcb7b207482093840ff4e482b3c1fba247f100a
RJ Buxton <buxrj14@gmail.com>
2022-11-04 19:03:12 -0400

    Assigment 3 part 1 changes

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -16,6 +22,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+	int exit_val = system(cmd);
+
+	if(exit_val != 0)
+		return false;
 
     return true;
 }
@@ -58,6 +68,20 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	int status;
+	pid_t pid = fork();
+	if(pid == -1)
+		return false;
+	else if(pid == 0)
+	{
+		int err = execv(command[0], command);
+		exit(err);
+	}
+
+	wait(&status);
+
+	if(WEXITSTATUS(status) != 0)
+		return false;
 
     va_end(args);
 
@@ -92,6 +116,26 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+	int status;
+	int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	if(fd <0) return false;
+	pid_t pid = fork();
+	if(pid == -1)
+		return false;
+	else if(pid == 0)
+	{
+		if(dup2(fd, 1) < 0) return false;
+		int err = execv(command[0], command);
+		exit(err);
+	}
+
+	close(fd);
+
+	wait(&status);
+
+	if(WEXITSTATUS(status) != 0)
+		return false;
+
 
     va_end(args);
 
